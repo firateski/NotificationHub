93d8af658116c260f4e2d0f1c22a05b9cdc94334
Firat Eski <frat888@gmail.com>
2020-09-10 03:42:25 +0300

    Rename 'AccessManagerUtil' to 'DateUtil'

--- src/main/java/Util/DateUtil.java
+++ src/main/java/Util/DateUtil.java
@@ -1,11 +1,13 @@
+package Util;
+
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
-public class AccessManagerUtils {
-    static String DATE_PATTERN = "MM-dd-yyyy";
+public class DateUtil {
+    public static String DATE_PATTERN = "MM-dd-yyyy";
 
     public static long getDaysBetweenDates(Date date1, Date date2){
         SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_PATTERN);
@@ -15,8 +17,6 @@ public class AccessManagerUtils {
         LocalDate localDate1 = LocalDate.parse(dateFormatter.format(date1), dtf);
         LocalDate localDate2 = LocalDate.parse(dateFormatter.format(date2), dtf);
 
-        long numberOfDaysBetween = ChronoUnit.DAYS.between(localDate1, localDate2);
-
-        return numberOfDaysBetween;
+        return ChronoUnit.DAYS.between(localDate1, localDate2);
     }
 }
--- src/test/java/Util/DateUtilTest.java
+++ src/test/java/Util/DateUtilTest.java
@@ -1,6 +1,5 @@
 package Util;
 
-import Util.AccessManagerUtils;
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -9,18 +8,18 @@ import java.util.Date;
 
 import static org.junit.Assert.assertTrue;
 
-public class AccessManagerUtilsTest {
+public class DateUtilTest {
     @Test
     public void should_return_equal_or_greater_than_30_days_for_given_dates(){
         Date date1 = null, date2 = null;
         try {
-            date1 = new SimpleDateFormat(AccessManagerUtils.DATE_PATTERN).parse("01-01-2020");
-            date2 = new SimpleDateFormat(AccessManagerUtils.DATE_PATTERN).parse("02-01-2020");
+            date1 = new SimpleDateFormat(DateUtil.DATE_PATTERN).parse("01-01-2020");
+            date2 = new SimpleDateFormat(DateUtil.DATE_PATTERN).parse("02-01-2020");
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        long output = AccessManagerUtils.getDaysBetweenDates(date1, date2);
+        long output = DateUtil.getDaysBetweenDates(date1, date2);
 
         assertTrue("Number of dates should equal or greater than 30 between dates",output >= 30);
     }
@@ -29,13 +28,13 @@ public class AccessManagerUtilsTest {
     public void should_return_lower_than_30_days_for_given_dates(){
         Date date1 = null, date2 = null;
         try {
-            date1 = new SimpleDateFormat(AccessManagerUtils.DATE_PATTERN).parse("01-01-2020");
-            date2 = new SimpleDateFormat(AccessManagerUtils.DATE_PATTERN).parse("01-29-2020");
+            date1 = new SimpleDateFormat(DateUtil.DATE_PATTERN).parse("01-01-2020");
+            date2 = new SimpleDateFormat(DateUtil.DATE_PATTERN).parse("01-29-2020");
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        long output = AccessManagerUtils.getDaysBetweenDates(date1, date2);
+        long output = DateUtil.getDaysBetweenDates(date1, date2);
 
         assertTrue("Number of dates should lower than 30 between dates",output < 30);
     }
